b4f0eb031dc964ab23e4d98a29c8f2312b9af44c
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2018-04-05 22:32:31 +0300

    Initial repo commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Trailings
+
+Trailings metrics - data stream procesing toolkit
+
+Install dependencies:
+
+```sh
+mvn install
+```
+
+Build package with dependencies:
+
+```sh
+mvn clean compile assembly:single
+```
+
+Run program cycle:
+
+```sh
+mvn package
+```
---
+++ src/main/java/com/wildbeeslabs/sensiblemetrics/trailings/AppLoader.java
@@ -0,0 +1,16 @@
+package com.wildbeeslabs.sensiblemetrics.trailings;
+
+/**
+ *
+ * Main Trailings Application class
+ *
+ * @author Alex
+ * @version 1.0.0
+ * @since 2017-12-12
+ */
+public class AppLoader {
+
+    public static void main(String[] args) {
+        new TrailingsProcessor().init(args);
+    }
+}
